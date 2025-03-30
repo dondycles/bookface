ad77dd1a412912abde644331fe6f7e9e7b39c075
dondycles <johnrod.dondoyano@gmail.com>
2025-03-31 00:13:26 +0800

    nav styles

--- src/lib/components/nav.tsx
+++ src/lib/components/nav.tsx
@@ -48,123 +48,125 @@ export default function Nav({
     }
   }, [searching, debouncedQ, route]);
   return (
-    <nav className="gap-4 flex items-center justify-between fixed w-full px-2 sm:px-4 py-4 z-10 bg-muted">
-      <div className="flex gap-2 flex-1 justify-start">
-        {searching ? (
-          <>
-            <Input
-              value={q}
-              onChange={(e) => {
-                setQ(e.currentTarget.value);
-              }}
-              className="max-w-[256px]"
-              autoFocus={true}
-              placeholder="Search for somethin'"
-            />
-            <Button
-              className="bg-accent"
-              size={"icon"}
-              onClick={() => {
-                setSearching(false);
-                setQ("");
-              }}
-              variant={"ghost"}
-            >
-              <X />
-            </Button>
-          </>
-        ) : (
-          <>
-            <Link to={"/feed"} className="text-4xl font-bold leading-none">
-              bookface
-            </Link>
-            <Button
-              key={"Search"}
-              onClick={() => setSearching(true)}
-              variant={"ghost"}
-              className="aspect-square h-9"
-              size={"icon"}
-            >
-              <Search className="size-6" />
-            </Button>
-          </>
-        )}
-      </div>
-      {currentUser ? (
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            <UserAvatar
-              url={currentUser.dB.image}
-              className="size-12"
-              alt={currentUser.dB.username ?? currentUser.dB.email}
-            />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem asChild>
-              <Link
-                to="/$username"
-                params={{ username: currentUser.dB.username as string }}
+    <nav className="gap-4  fixed w-full z-10 bg-muted">
+      <div className="sm:max-w-[512px] mx-auto flex items-center justify-between w-full  px-2 sm:px-0  py-4">
+        <div className="flex gap-2 flex-1 justify-start">
+          {searching ? (
+            <>
+              <Input
+                value={q}
+                onChange={(e) => {
+                  setQ(e.currentTarget.value);
+                }}
+                className="max-w-[256px]"
+                autoFocus={true}
+                placeholder="Search for somethin'"
+              />
+              <Button
+                className="bg-accent"
+                size={"icon"}
+                onClick={() => {
+                  setSearching(false);
+                  setQ("");
+                }}
+                variant={"ghost"}
               >
-                <Avatar>
-                  <AvatarImage
-                    src={currentUser.dB.image ?? "/favicon.ico"}
-                    alt="@shadcn"
-                  />
-                  <AvatarFallback>BF</AvatarFallback>
-                </Avatar>
-                <p>{currentUser.dB.name}</p>
+                <X />
+              </Button>
+            </>
+          ) : (
+            <>
+              <Link to={"/feed"} className="text-4xl font-bold leading-none">
+                bookface
               </Link>
-            </DropdownMenuItem>
-            <DropdownMenuSub>
-              <AddPostDialog>
-                <DropdownMenuSubTrigger
-                  showIcon={false}
-                  className="p-2 flex gap-2 cursor-pointer"
+              <Button
+                key={"Search"}
+                onClick={() => setSearching(true)}
+                variant={"ghost"}
+                className="aspect-square h-9"
+                size={"icon"}
+              >
+                <Search className="size-6" />
+              </Button>
+            </>
+          )}
+        </div>
+        {currentUser ? (
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <UserAvatar
+                url={currentUser.dB.image}
+                className="size-12"
+                alt={currentUser.dB.username ?? currentUser.dB.email}
+              />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem asChild>
+                <Link
+                  to="/$username"
+                  params={{ username: currentUser.dB.username as string }}
                 >
-                  <Plus className="size-4" />
-                  <p>New Post</p>
-                </DropdownMenuSubTrigger>
-              </AddPostDialog>
-            </DropdownMenuSub>
+                  <Avatar>
+                    <AvatarImage
+                      src={currentUser.dB.image ?? "/favicon.ico"}
+                      alt="@shadcn"
+                    />
+                    <AvatarFallback>BF</AvatarFallback>
+                  </Avatar>
+                  <p>{currentUser.dB.name}</p>
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuSub>
+                <AddPostDialog>
+                  <DropdownMenuSubTrigger
+                    showIcon={false}
+                    className="p-2 flex gap-2 cursor-pointer"
+                  >
+                    <Plus className="size-4" />
+                    <p>New Post</p>
+                  </DropdownMenuSubTrigger>
+                </AddPostDialog>
+              </DropdownMenuSub>
 
-            <DropdownMenuSeparator />
-            <DropdownMenuItem asChild>
-              <Link to="/settings">
-                <Settings />
-                <p>Account Settings</p>
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem
-              onClick={async () => {
-                await authClient.signOut();
-                await queryClient.invalidateQueries({ queryKey: ["currentUser"] });
-                await queryClient.invalidateQueries({
-                  queryKey: ["user", currentUser.dB.username],
-                });
-                await router.invalidate();
-              }}
-            >
-              <LogOut className="text-destructive" />
-              <p>Log Out</p>
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <ThemeToggle />
-          </DropdownMenuContent>
-        </DropdownMenu>
-      ) : (
-        <Button
-          onClick={() =>
-            authClient.signIn.social({
-              provider: "google",
-              callbackURL: "/feed",
-            })
-          }
-          size={"icon"}
-        >
-          <LogIn />
-        </Button>
-      )}
-      <SetUsernameDialog currentUser={currentUser} />
+              <DropdownMenuSeparator />
+              <DropdownMenuItem asChild>
+                <Link to="/settings">
+                  <Settings />
+                  <p>Account Settings</p>
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                onClick={async () => {
+                  await authClient.signOut();
+                  await queryClient.invalidateQueries({ queryKey: ["currentUser"] });
+                  await queryClient.invalidateQueries({
+                    queryKey: ["user", currentUser.dB.username],
+                  });
+                  await router.invalidate();
+                }}
+              >
+                <LogOut className="text-destructive" />
+                <p>Log Out</p>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <ThemeToggle />
+            </DropdownMenuContent>
+          </DropdownMenu>
+        ) : (
+          <Button
+            onClick={() =>
+              authClient.signIn.social({
+                provider: "google",
+                callbackURL: "/feed",
+              })
+            }
+            size={"icon"}
+          >
+            <LogIn />
+          </Button>
+        )}
+        <SetUsernameDialog currentUser={currentUser} />
+      </div>
     </nav>
   );
 }
